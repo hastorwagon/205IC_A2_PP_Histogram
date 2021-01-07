a4c1c02a85c842f16a837de96cac6e7252b4a94f
Mateusz O <75068608+hastorwagon@users.noreply.github.com>
2021-01-07 17:09:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// 1. Wypelnić tablice o zadanym rozmiarze lcizbami pseudolosowymi (int) z zadanego przedziału
-// 2. Policzyć sume elementów tablicy
-// 3. Policzyć sredni arytmetyzna elementow tablicy
-// 4. wygeneruje histogram wartosci tablicy
 
-#define ROZMIAR_TABLICY 10
+#define ROZMIAR_TABLICY 15
 #define MIN 2
 #define MAX 10
+#define ROZMIAR_HISTOGRAMA 11
 
-
-void wypelnijTabliceLiczbamiPseudolosowymi(int* tab, int min, int max);
+void wypelnijTablice(int* tab, int min, int max);
 int sumaTablicy(int *tab);
 float sredniaTablicy(int *tab);
 void generujHistogram(int *tab, int* histogram);
@@ -20,40 +16,56 @@ void wypiszStatystyki(int *tab, int suma, float srednia, int* histogram);
 
 int main() {
     int tablica[ROZMIAR_TABLICY];
-    wypelnijTabliceLiczbamiPseudolosowymi(tablica, MIN, MAX);
+    int histogram[ROZMIAR_HISTOGRAMA];
+    for(int i=0; i<ROZMIAR_HISTOGRAMA; i++ ){
+        histogram[i]=0;
+    }
+    wypelnijTablice(tablica, MIN, MAX);
+    generujHistogram(tablica, histogram);
     wypiszTablice(tablica);
-    //wypiszStatystyki();
+    wypiszStatystyki (tablica, sumaTablicy(tablica), sredniaTablicy(tablica), histogram);
     return 0;
 }
 
-void wypelnijTabliceLiczbamiPseudolosowymi(int* tab, int min, int max){
+void wypelnijTablice(int* tab, int min, int max){
     srand(time(0));
 
     for (int i=0; i<ROZMIAR_TABLICY; i++) {
-        tab[i] = rand() % max;
-       // sumaTablicy = min + max;
+        tab[i] = MIN + rand() % (MAX - MIN+1);
     }
 }
 
+int sumaTablicy(int *tab){
+    int suma=0;
+    for(int i=0; i<ROZMIAR_TABLICY; i++) {
+        suma = suma+tab[i];
+    }
+    return suma;
+}
 
+float sredniaTablicy(int *tab) {
+    float srednia = 0;
+    srednia = sumaTablicy(tab) / ROZMIAR_TABLICY;
+    return srednia;
+}
+void generujHistogram(int *tab, int* histogram) {
+    for (int i = 0; i < ROZMIAR_TABLICY; i++) {
+        histogram[tab[i]] = histogram[tab[i]] + 1;
+    }
+}
 
 void wypiszTablice(int *tab) {
-    printf("\n");
-    for (int i=0; i<ROZMIAR_TABLICY; i++) {
-        printf("%d ", tab[i]);
-    }
-}
-
-
-/*
-void generujHistogram(int *tab, int* histogram);
-    int i, j;
-    for (i = 0; i<ROZMIAR_TABLICY; i++) {
-        printf("[%d]", i);
-        for ( j = 0; j < hist[i]; ++j) {
-            printf("*");
+        printf("\n[");
+        for (int i = 0; i < ROZMIAR_TABLICY; i++) {
+            printf("%d ", tab[i]);
         }
-        printf("\n");
-    }
+        printf("]\n");
 }
- */
+
+void wypiszStatystyki(int *tab, int suma, float srednia, int* histogram) {
+        printf("Suma: %d \n", suma);
+        printf("Srednia: %.1f \n", srednia);
+        for (int i = 0; i < ROZMIAR_HISTOGRAMA; i++) {
+            printf("%d -> %d \n", i, histogram[i]);
+        }
+    }
